6fd5d1d87d73215c8f3d69b40ff02718cc9439d0
Jan <raytracer@localhost>
2018-05-11 22:10:11 +0200

    Added Dockerfile and fixed previewScript

--- helper-scripts/CreateOrUpdatePreviewImages.py
+++ helper-scripts/CreateOrUpdatePreviewImages.py
@@ -2,8 +2,9 @@ import os
 import json
 from PIL import Image
 import time
+import shutil
 
-fileEndings = ['jpg', 'jpeg']
+fileEndings = ['jpg', 'jpeg', 'png']
 
 def CreateOrUpdatePreviewImages(sourceFolder, previewFolder):
     progressCounter = 0
@@ -12,13 +13,13 @@ def CreateOrUpdatePreviewImages(sourceFolder, previewFolder):
        
         CreateDirectoryInfo(sourcePath, previewPath, files, subDirs)
 
-        #DeleteObsoleteImagesOrFolderInPreview(previewPath, files, subDirs)
+        DeleteObsoleteImagesOrFolderInPreview(previewPath, files, subDirs)
 
         for img in files:
             progressCounter += 1
             print("Progress: " + str(progressCounter))
 
-            if img.split('.')[-1] not in fileEndings:
+            if img.split('.')[-1].lower() not in fileEndings:
                 continue
 
             srcImgPath = os.path.join(sourcePath, img)
@@ -31,10 +32,12 @@ def CreateOrUpdatePreviewImages(sourceFolder, previewFolder):
 
 def CreatePreviewImage(srcImgPath, previewImgPath):
     print("Create preview: " + srcImgPath + " - " + previewImgPath)
-
-    im = Image.open(srcImgPath)
-    im.thumbnail((512, 512))
-    im.save(previewImgPath)
+    try:
+        im = Image.open(srcImgPath)
+        im.thumbnail((512, 512))
+        im.save(previewImgPath, quality=50)
+    except:
+        print("Could not create preview of the following file: " + srcImgPath)
 
 def DeleteObsoleteImagesOrFolderInPreview(previewPath, srcFiles, srcDirs):
     for f in os.listdir(previewPath):
@@ -44,27 +47,33 @@ def DeleteObsoleteImagesOrFolderInPreview(previewPath, srcFiles, srcDirs):
             continue
         if f in srcDirs:
             continue
-        os.remove(os.path.join(previewPath, f))
+        f = os.path.join(previewPath, f)
+        print("Remove: " + f)
+        if os.path.isfile(f):
+            os.remove(f)
+        else:
+            shutil.rmtree(f)
 
 def CreateDirectoryInfo(sourcePath, previewPath, files, subDirs):
-    dirInfo = {"name" : sourcePath.split("\\")[-1], "sourcePath" : sourcePath, "previewPath" : previewPath, "files" : files, "subDirectories" : subDirs }
+    dirInfo = {"name" : os.path.basename(os.path.normpath(sourcePath)), "sourcePath" : sourcePath, "previewPath" : previewPath, "files" : files, "subDirectories" : subDirs }
 
     dump = json.dumps(dirInfo, sort_keys=True, indent=3, separators=(',',': '))
 
     if not os.path.exists(previewPath):
         os.makedirs(previewPath)
 
-    with open(previewPath + "\\info.json", "w") as textFile:
+    with open(os.path.join(previewPath, "info.json"), "w") as textFile:
         textFile.write(dump)
 
-    
+
 print("Updating the preview images...")
 
 start = time.time()
 
-CreateOrUpdatePreviewImages("C:\\Users\\Jan.Reinhardt\\Pictures\\src", "C:\\Users\\Jan.Reinhardt\\Pictures\\preview")
+CreateOrUpdatePreviewImages("/var/www/data/images", "/var/www/data/preview")
 
 end = time.time()
 
 print("Finished updating the preview images!")
 print("The operation took " + str(end-start) + "s")
+
